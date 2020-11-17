a84218dccefa47b93277cfb97451ca802db49447
stevenacz <scoailazaa@gmail.com>
2020-11-17 15:32:49 -0500

    Problema 24

--- 24contact/contact.rb
+++ 24contact/contact.rb
@@ -1,3 +1,17 @@
 class Contact
-  # your code here
+  attr_reader :name, :email
+
+  def initialize(name, email)
+    @name = name
+    @email = email
+  end
+
+  def to_s
+    "#{name},#{email}"
+  end
 end
+
+# contacto = Contact.new("John Doe", "john@mail.com")
+# p contacto.name
+# p contacto.email
+# p contacto.to_s
