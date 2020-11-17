9acc073117f9b6d0b15f09a784d71633c03a0ad6
stevenacz <scoailazaa@gmail.com>
2020-11-17 15:32:13 -0500

    Problema 25

--- 25agenda/agenda.rb
+++ 25agenda/agenda.rb
@@ -1,18 +1,53 @@
 require_relative "../24contact/contact"
 
 class Agenda
+  attr_reader :filename, :contacts
+
   def initialize(filename)
     @filename = filename
     @contacts = load_contacts
   end
 
-  # your code here
+  def add(name, email)
+    name.match(/\w+/) && email.match(/\A[\w.+-]+@\w+\.\w+\z/) && @contacts.push(Contact.new(name, email))
+  end
+
+  def save
+    text = ""
+    @contacts.each do |contact|
+      text += "#{contact.name},#{contact.email}\n"
+    end
+    File.write(filename, text, mode: "w")
+  end
+
+  def filter(field, term)
+    @filter_contacts = []
+    @filter_contacts = case field
+                       when "name" then filter_name(term)
+                       when "email" then filter_email(term)
+                       end
+  end
 
   private
 
   def load_contacts
-    # your code here
+    if File.exist?(@filename)
+      contacts = []
+      File.read(filename).split("\n").each do |item|
+        contact_arr = item.split(",")
+        contacts.push(Contact.new(contact_arr[0], contact_arr[1]))
+      end
+      contacts
+    else
+      File.write(@filename, nil, mode: "w")
+    end
   end
 
-  # you can add more auziliary methods here
+  def filter_name(term)
+    @contacts.select { |contact| contact.name.match(/#{term}/) }
+  end
+
+  def filter_email(term)
+    @contacts.select { |contact| contact.email.match(/#{term}/) }
+  end
 end
