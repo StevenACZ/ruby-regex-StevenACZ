5a826a1300240b57fa892c48e2cbe8bb55c1df04
stevenacz <scoailazaa@gmail.com>
2020-11-17 15:31:14 -0500

    Problema 26

--- 26manager/manager.rb
+++ 26manager/manager.rb
@@ -6,13 +6,55 @@ class AgendaManager
   end
 
   def start
-    # your code here
+    until menu
+      option = gets.chomp
+      case option
+      when "list" then list
+      when "add" then add
+      when "save" then save
+      when "search" then search
+      when "exit" then break
+      end
+    end
   end
 
   private
 
   def menu
-    # your code here
+    puts "-----------------------#{@agenda.filename}-----------------------"
+    puts "1. list     2. add      3. save     4. search   5. exit"
+  end
+
+  def list
+    @agenda.contacts.each do |contact|
+      puts "#{contact.name},#{contact.email}"
+    end
+  end
+
+  def add
+    print "Name: "
+    name = gets.chomp
+    print "Email: "
+    email = gets.chomp
+    if @agenda.add(name, email).nil?
+      puts "Failed"
+    else
+      puts "Contact added"
+    end
+  end
+
+  def save
+    @agenda.save
+  end
+
+  def search
+    print "Field: "
+    field = gets.chomp
+    print "Search term: "
+    term = gets.chomp
+    @agenda.filter(field, term).each do |contact|
+      puts "#{contact.name},#{contact.email}"
+    end
   end
 end
 
